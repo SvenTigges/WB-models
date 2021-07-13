1b79df4bb275dce11d215c2dd1d9fbff5c038a7b
SvenTigges <84066413+SvenTigges@users.noreply.github.com>
2021-07-13 12:49:53 +0200

    Create 03_tabs_w_inserts.sql

---
+++ 01_FirstModel/sql/03_tabs_w_inserts.sql
@@ -0,0 +1,25 @@
+-- 3. verbundene Tabellen + Inserts
+
+-- Vorbereitung
+DROP DATABASE IF EXISTS mydb;
+CREATE DATABASE mydb;
+USE mydb;
+
+-- MAstertabelle: unverändert
+CREATE TABLE IF NOT EXISTS `mydb`.`cats` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cat_name` VARCHAR(45) NOT NULL,
+  `fur_color` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+-- Struktur: MT
+DESCRIBE cats;
+
+-- Inserts: MT 
+INSERT INTO `mydb`.`cats` (`id`, `cat_name`, `fur_color`) VALUES (DEFAULT, "Grizabella", "white");
+INSERT INTO `mydb`.`cats` (`id`, `cat_name`, `fur_color`) VALUES (DEFAULT, "Alonzo", "grey");
+INSERT INTO `mydb`.`cats` (`id`, `cat_name`, `fur_color`) VALUES (DEFAULT, "Mausi", "striped");
+
+-- Inhalte: MT 
+SELECT * FROM cats; 
